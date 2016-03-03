685f9d573a3e54e3faa74f61c4cd655d82477194
Nico Janssens <nico.nj.janssens@alcatel-lucent.com>
2016-03-03 15:24:43 -0500

    Replaced winston w debug logging to be compatible w other project

--- examples/peer.js
+++ examples/peer.js
@@ -2,7 +2,6 @@
 
 var dgram = require('dgram')
 var UdpHolePuncher = require('../index')
-var winston = require('winston')
 
 // args
 var argv = require('yargs')
@@ -23,41 +22,34 @@ var argv = require('yargs')
   .nargs('p', 1)
   .default('p', '12345')
   .describe('p', 'public peer port')
-  // log level
-  .default('l', 'debug')
-  .choices('l', ['info', 'debug', 'warn', 'error', 'verbose', 'silly'])
-  .alias('l', 'log')
-  .nargs('l', 1)
-  .describe('l', 'Log level')
   // help
   .help('h')
   .alias('h', 'help')
   .argv
 
 var dataMessages = 10
-winston.level = argv.log
 
 // socket configuratio
 var socket = dgram.createSocket('udp4')
 socket.on('error', function (error) {
-  winston.error('socket error:\n' + error.stack)
+  console.error('socket error:\n' + error.stack)
   socket.close()
 })
 socket.on('message', function (message, rinfo) {
   var data = message.toString()
-  winston.info('receiving ' + data + ' from ' + rinfo.address + ':' + rinfo.port)
+  console.log('receiving ' + data + ' from ' + rinfo.address + ':' + rinfo.port)
 })
 socket.on('listening', function () {
   var address = socket.address()
-  winston.info('listening at ' + address.address + ':' + address.port)
+  console.log('listening at ' + address.address + ':' + address.port)
   // puncher configuration
   var puncher = new UdpHolePuncher(socket)
   puncher.on('connected', function () {
-    winston.info('woohoo, we can talk to ' + argv.addr + ':' + argv.port)
+    console.log('woohoo, we can talk to ' + argv.addr + ':' + argv.port)
     sendData()
   })
   puncher.on('error', function (error) {
-    winston.info('woops, something went wrong: ' + error)
+    console.log('woops, something went wrong: ' + error)
   })
   puncher.connect(argv.addr, argv.port)
 })
@@ -68,7 +60,7 @@ socket.bind(argv.bind)
 function sendData () {
   for (var i = 0; i < dataMessages; i++) {
     var data = 'message ' + i
-    winston.info('sending ' + data + ' to ' + argv.addr + ':' + argv.port)
+    console.log('sending ' + data + ' to ' + argv.addr + ':' + argv.port)
     var message = new Buffer(data)
     socket.send(message, 0, message.length, argv.port, argv.addr)
   }
