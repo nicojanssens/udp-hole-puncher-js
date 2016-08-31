3ca7e11b1225a55146f50d1907f628247ea4f10e
Nico Janssens <nico.b.janssens@gmail.com>
2016-08-31 22:00:07 +0200

    saved cordova-plugin-chrome-apps-sockets-udp plugin

--- test/browserify/cordova-app/start.sh
+++ test/browserify/cordova-app/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cordova platform add ios
-cordova plugin add https://github.com/MobileChromeApps/cordova-plugin-chrome-apps-sockets-udp
+cordova plugin add --save https://github.com/MobileChromeApps/cordova-plugin-chrome-apps-sockets-udp
 cordova build ios
 cordova emulate ios
